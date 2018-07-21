570d9cc658e24d541e0030cde1c814c09e7157ab
Alex Bogovich <bogovich.alexander@gmail.com>
2018-07-21 23:24:46 +0700

    Add device group query
    Signed-off-by: Alex Bogovich <bogovich.alexander@gmail.com>

--- src/main/scala/com/github/alexbogovich/actor/iot/DeviceGroup.scala
+++ src/main/scala/com/github/alexbogovich/actor/iot/DeviceGroup.scala
@@ -1,15 +1,24 @@
 package com.github.alexbogovich.actor.iot
 
-import akka.actor
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
-import com.github.alexbogovich.actor.iot.DeviceGroup.{ReplyDeviceList, RequestDeviceList}
+import com.github.alexbogovich.actor.iot.DeviceGroup.{ReplyDeviceList, RequestAllTemperatures, RequestDeviceList}
 import com.github.alexbogovich.actor.iot.DeviceManager.RequestTrackDevice
+import scala.concurrent.duration._
 
 object DeviceGroup {
   def props(groupId: String): Props = Props(new DeviceGroup(groupId))
 
   final case class RequestDeviceList(requestId: Long)
   final case class ReplyDeviceList(requestId: Long, ids: Set[String])
+
+  final case class RequestAllTemperatures(requestId: Long)
+  final case class RespondAllTemperatures(requestId: Long, temperatures: Map[String, TemperatureReading])
+
+  sealed trait TemperatureReading
+  final case class Temperature(value: Double) extends TemperatureReading
+  case object TemperatureNotAvailable extends TemperatureReading
+  case object DeviceNotAvailable extends TemperatureReading
+  case object DeviceTimedOut extends TemperatureReading
 }
 
 class DeviceGroup(groupId: String) extends Actor with ActorLogging {
@@ -49,5 +58,12 @@ class DeviceGroup(groupId: String) extends Actor with ActorLogging {
       actorToDeviceId -= deviceActor
       deviceIdToActor -= deviceId
 
+    case RequestAllTemperatures(requestId) ⇒
+      context.actorOf(DeviceGroupQuery.props(
+        actorToDeviceId = actorToDeviceId,
+        requestId = requestId,
+        requester = sender(),
+        3.seconds
+      ))
   }
 }
--- src/test/scala/com/github/alexbogovich/actor/iot/DeviceGroupTest.scala
+++ src/test/scala/com/github/alexbogovich/actor/iot/DeviceGroupTest.scala
@@ -99,4 +99,37 @@ class DeviceGroupTest extends TestKit(ActorSystem("MySpec")) with ImplicitSender
       probe.expectMsg(DeviceGroup.ReplyDeviceList(requestId = 1, Set("device2")))
     }
   }
+
+  "be able to collect temperatures from all active devices" in {
+    val probe = TestProbe()
+    val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor1 = probe.lastSender
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor2 = probe.lastSender
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device3"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor3 = probe.lastSender
+
+    // Check that the device actors are working
+    deviceActor1.tell(Device.RecordTemperature(requestId = 0, 1.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 0))
+    deviceActor2.tell(Device.RecordTemperature(requestId = 1, 2.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+    // No temperature for device3
+
+    groupActor.tell(DeviceGroup.RequestAllTemperatures(requestId = 0), probe.ref)
+    probe.expectMsg(
+      DeviceGroup.RespondAllTemperatures(
+        requestId = 0,
+        temperatures = Map(
+          "device1" -> DeviceGroup.Temperature(1.0),
+          "device2" -> DeviceGroup.Temperature(2.0),
+          "device3" -> DeviceGroup.TemperatureNotAvailable)))
+  }
 }
