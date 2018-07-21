e7d1d250658e037680d47c550d872a931be8c941
Alex Bogovich <bogovich.alexander@gmail.com>
2018-07-21 21:06:03 +0700

    Add request
    Signed-off-by: Alex Bogovich <bogovich.alexander@gmail.com>

--- src/test/scala/com/github/alexbogovich/actor/iot/DeviceTest.scala
+++ src/test/scala/com/github/alexbogovich/actor/iot/DeviceTest.scala
@@ -25,6 +25,26 @@ class DeviceTest extends TestKit(ActorSystem("MySpec")) with ImplicitSender
       response.requestId should ===(42)
       response.value should ===(None)
     }
+  }
 
+  "reply with latest temperature reading" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
+
+    deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+    deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
+    val response1 = probe.expectMsgType[Device.RespondTemperature]
+    response1.requestId should ===(2)
+    response1.value should ===(Some(24.0))
+
+    deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+
+    deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
+    val response2 = probe.expectMsgType[Device.RespondTemperature]
+    response2.requestId should ===(4)
+    response2.value should ===(Some(55.0))
   }
 }
