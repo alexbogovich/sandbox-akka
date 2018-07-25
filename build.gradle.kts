0a13581ef52ca4cd8dcee0d4868d66d6f09f75cc
Alexander Bogovich <bogovich.alexander@gmail.com>
2018-07-25 17:29:17 +0700

    add junit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,21 +11,29 @@ repositories {
 }
 
 dependencies {
-    compile ("org.scala-lang:scala-library:2.12.6")
-    compile ("com.typesafe.akka:akka-stream_2.12:2.5.14")
-    compile ("com.typesafe.akka:akka-actor-typed_2.12:2.5.14")
-    compile ("com.typesafe.akka:akka-http_2.12:10.1.3")
-    compile ("com.typesafe.akka:akka-http-spray-json_2.12:10.1.3")
+    compile("org.scala-lang:scala-library:2.12.6")
+    compile("com.typesafe.akka:akka-stream_2.12:2.5.14")
+    compile("com.typesafe.akka:akka-actor-typed_2.12:2.5.14")
+    compile("com.typesafe.akka:akka-http_2.12:10.1.3")
+    compile("com.typesafe.akka:akka-http-spray-json_2.12:10.1.3")
 
-    testCompile ("org.scalatest:scalatest_2.12:3.0.5")
-    testCompile ("com.typesafe.akka:akka-stream-testkit_2.12:2.5.14")
-    testCompile ("com.typesafe.akka:akka-actor-testkit-typed_2.12:2.5.14")
-    testCompile ("junit:junit:4.12")
+    testCompile("org.scalatest:scalatest_2.12:3.0.5")
+    testCompile("com.typesafe.akka:akka-stream-testkit_2.12:2.5.14")
+    testCompile("com.typesafe.akka:akka-actor-testkit-typed_2.12:2.5.14")
+
+    testCompile("org.junit.platform:junit-platform-runner:1.2.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
 }
 
-tasks.withType<Wrapper> {
-    gradleVersion = "4.9"
-    distributionType = Wrapper.DistributionType.ALL
+tasks.run {
+    withType<Wrapper> {
+        gradleVersion = "4.9"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 buildScan {
